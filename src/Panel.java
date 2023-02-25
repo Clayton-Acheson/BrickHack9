eab6aa56dd4cb2047c1d0d4dbdf3456419f21e39
cgr9496 <cgr9496@rit.edu>
2023-02-25 15:30:44 -0500

    tertiary Push for simplification

--- src/Panel.java
+++ src/Panel.java
@@ -14,36 +14,23 @@ public class Panel extends JFrame {
     public Panel() {
         super("Virus Fighter 199X");
 
-<<<<<<< Updated upstream
 
         setSize(width, height - (height/20));
-=======
-    public Panel(int rows, int columns) {
-        super("Game Name Pending");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(width, height);
->>>>>>> Stashed changes
         setLocationRelativeTo(null);
 
         JPanel titlePanel = new JPanel();
         titlePanel.setLayout(new BorderLayout());
         titlePanel.setSize(width, height/2);
 
-        JPanel adPanel = new JPanel();
-        adPanel.setLayout(new CardLayout());
-        /*adPanel.setLayout(new GridLayout(rows, (int) (columns*(.6))));
-        for (int i = 0; i < rows*(.3); i++) {
-            for (int j = 0; j < columns; j++) {
-                adPanel.add(GUI.createButton("ad placeholder"));
-            }
-<<<<<<< Updated upstream
-        } */
+        JPanel cardPanel = new JPanel();
+        CardLayout cardLayout = new CardLayout();
+        cardPanel.setLayout(cardLayout);
+        cardPanel.add(GUI.createButton("Card 1"), "Card 1");
+        cardPanel.add(GUI.createButton("Card 2"), "Card 2");
+
 
         titlePanel.add(GUI.createLabel("Virus Fighter 19XX"), BorderLayout.CENTER);
-=======
-        }
->>>>>>> Stashed changes
-        adPanel.setBackground(Color.BLACK);
+        cardPanel.setBackground(Color.BLACK);
         titlePanel.setBackground(Color.GRAY);
 
         JPanel levelPanel = new JPanel();
@@ -57,7 +44,7 @@ public class Panel extends JFrame {
         }
 
         add(titlePanel, BorderLayout.NORTH);
-        add(adPanel, BorderLayout.CENTER);
+        add(cardPanel, BorderLayout.CENTER);
         add(levelPanel, BorderLayout.WEST);
 
         setVisible(true);
