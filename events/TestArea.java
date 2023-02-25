d16a40e282b55b3a49c12d3e4f233b014870803a
ClaytonAcheson <cracheson16@gmail.com>
2023-02-25 18:19:46 -0500

    Added TestArea
    Did the thing

---
+++ events/TestArea.java
@@ -0,0 +1,15 @@
+
+ import javax.swing.*;
+ import java.awt.Dimension;
+ import java.awt.Color;
+ import java.awt.Graphics;
+  
+ public class TestArea extends JLabel {
+  
+     public TestArea(Color color, String text) {
+         setBackground(color);
+         setOpaque(true);
+         setBorder(BorderFactory.createEmptyBorder());
+         setText(text);
+     }
+ }
